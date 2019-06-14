1e2f59f9438a94363e3f64df1dcb0c08a194dbde
Manik Jain <39870304+manikjain07@users.noreply.github.com>
2019-06-14 18:06:12 +0530

    Update SumFunction.py

--- SumFunction.py
+++ SumFunction.py
@@ -1,11 +1,11 @@
 def main():
     number1 = int(input("Enter a Number: "))
     number2 = int(input("Enter another Number: "))
-    sum()
+    sum(number1,number2)
 
 def sum(number1,number2):
-    number_sum = number1 + number2
-    print("The sum of %d and %d is %d" %(number1,number2,number_sum))
+    number_sum = int(number1) + int(number2)
+    print("The sum of %s and %s is %d." %(number1,number2,number_sum))
 
 if __name__ == '__main__':main()
 
